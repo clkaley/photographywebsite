f8908416b69b74f2b3fba75df69b62666f06136b
Aleyna <74673470+clkaley@users.noreply.github.com>
2020-12-25 18:04:23 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+
+
+            var menubtn=document.getElementById("menubtn")
+            var side=document.getElementById("side")
+            var menu=document.getElementById("menu")
+            
+            menubtn.onclik=function(){
+                 if (side.style.right="-250px"){
+                 side.style.right="0";
+                 }
+                 else{
+                 side.style.right="-250";
+                 }
+} 
+
